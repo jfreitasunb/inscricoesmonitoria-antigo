45182e7c4a0277a25227bece6aaf7005d6247845
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-23 17:35:25 -0200

    Rotina para gravar escolhas dos candidatos à monitoria.

--- actions/processa_monitoria.php
+++ actions/processa_monitoria.php
@@ -4,66 +4,107 @@ $disciplinas_escolhidas = $_POST;
  
 var_dump($disciplinas_escolhidas);
 
+var_dump($_FILES);
+
 $errors = valida_escolhas_aluno($disciplinas_escolhidas);
 
 $id_candidato = 1;
 $ano_monitoria = '2017';
 $semestre_monitoria = '1';
 
-// if (empty($errors)){
-//     grava_escolhas_monitor($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas);
-// }else{
-//     print_r($errors);
-// }
+if (empty($errors)){
+    $resultado = grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas);
+    
+    if (in_array(0, $resultado)) {
+        $errors[] = "Houve um erro na hora de gravar suas escolhas. Tente novamente mais tarde.";
+    }else{
+        $_UP['pasta'] = "uploads";
+ 
+// Tamanho máximo do arquivo (em Bytes)
+$_UP['tamanho'] = 1024 * 1024 * 5; // 2Mb
+ 
+// Array com as extensões permitidas
+$_UP['extensoes'] = array("jpg","jpeg", "pdf", "png");
+     
+// Renomeia o arquivo? (Se true, o arquivo será salvo como .jpg e um nome único)
+$_UP['renomeia'] = true;
+ 
+// Array com os tipos de erros de upload do PHP
+$_UP['erros'][0] = 'N&atilde;o houve erro';
+$_UP['erros'][1] = 'O arquivo no upload &eacute; maior do que o limite do PHP';
+$_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
+$_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
+$_UP['erros'][4] = 'N&atilde;o foi feito o upload do arquivo';
 
 
-$campos = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, ano_monitoria_ativa, semestre_monitoria_ativa';
-
-$bind_valores = ':id_candidato, :escolha_aluno, :mencao_aluno, :ano_cursado, :semestre_cursado, :ano_monitoria_ativa, :semestre_monitoria_ativa';
-
-$campos2 = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, tipomonitoria, nome_hora_monitoria, concordatermos, ano_monitoria_ativa, semestre_monitoria_ativa';
-
-$bind_valores2 = ':id_candidato, :escolha_aluno, :mencao_aluno, :ano_cursado, :semestre_cursado, :tipomonitoria, :nome_hora_monitoria, :concordatermos, :ano_monitoria_ativa, :semestre_monitoria_ativa';
-
-$ano_monitoria_ativa = $ano_monitoria;
-$semestre_monitoria_ativa = $semestre_monitoria;
-
-$escolheu_hora = horarios_escolhidos_candidato($disciplinas_escolhidas);
+// tabela admin_anexos: id_documento, numero_anexo, nome_arquivo
 
 
-
-foreach ($escolheu_hora as $key) {
-    $hora_escolhida .=$disciplinas_escolhidas[$key];
+    if ($_FILES['arquivo']['name']!="")
+        {
+        //############### CASO ESTEJA PRONTO PARA UPLOAD
+        // Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
+        if ($_FILES['arquivo']['error'] != 0) 
+            {
+            $errors[] = "Não foi possível enviar seu arquivo.";
+            exit; // Para a execução do script
+            }
+        // Caso script chegue a esse ponto, não houve erro com o upload e o PHP pode continuar
+     
+        // Faz a verificação da extensão do arquivo
+        $extensao_00=explode(".",$_FILES['arquivo']['name']);
+        $extensao=strtolower(end($extensao_00));
+        if (array_search($extensao, $_UP['extensoes']) === false) 
+            {
+            $errors[]= "Por favor, envie arquivos com as seguintes extens&otilde;es: tex";
+            }
+     
+        // Faz a verificação do tamanho do arquivo
+            else if ($_UP['tamanho'] < $_FILES['arquivo']['size']) 
+            {
+            $errors[] = "O arquivo enviado &eacute; muito grande, envie arquivos de at&eacute; 2Mb.";
+            }
+     
+        // O arquivo passou em todas as verificações, hora de tentar movê-lo para a pasta
+            else {
+        // Primeiro verifica se deve trocar o nome do arquivo
+                if ($_UP['renomeia'] == true) 
+                    {
+        // Cria um nome baseado na quantidade de itens armazenados: 
+                    $nome_final = $aval."_cespe";
+                    //echo $nome_final;
+                    } else 
+                    {
+        // Mantém o nome original do arquivo
+                    $nome_final = $_FILES['arquivo']['name'];
+                    }
+     
+        // Depois verifica se é possível mover o arquivo para a pasta escolhida
+                if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $_UP['pasta'] .$nome_final.".csv")) 
+                    {
+        // Upload efetuado com sucesso, exibe uma mensagem e um link para o arquivo; insere ponteiro na tabela
+                    echo "Upload efetuado com sucesso!";
+                    
+                    } 
+                    else 
+                    {
+        // Não foi possível fazer o upload, provavelmente a pasta está incorreta
+                    $errors[] = "N&atilde;o foi poss&iacute;vel enviar o arquivo, tente novamente";
+                    }
+     
+                 }
+        
+        }
+    }
+}else{
+    print_r($errors);
 }
 
-$campos_finais = 'id_candidato, tipomonitoria, hora_escolhida, concordatermos, historico, ano_monitoria_ativa, semestre_monitoria_ativa, finaliza_escolhas';
+print_r($errors);
 
-$bind_valores_finais = ':id_candidato, :tipomonitoria, :hora_escolhida, :concordatermos, :historico, :ano_monitoria_ativa, :semestre_monitoria_ativa, :finaliza_escolhas';
 
-$query_insere_escolha_aluno = "INSERT INTO escolhas_candidatos ($campos) VALUES($bind_valores)";
-$stmt = $PDO->prepare( $query_insere_escolha_aluno );
-$stmt -> bindParam(':id_candidato', $id_candidato);
-$stmt -> bindParam(':tipo_monitoria', $disciplinas_escolhidas['tipo_monitoria']);
-$stmt -> bindParam(':hora_escolhida', $hora_escolhida);
-$stmt -> bindParam(':concordatermos', $disciplinas_escolhidas['concordatermos']);
-$stmt -> bindParam(':historico', $disciplinas_escolhidas['historico']);
-$stmt -> bindParam(':ano_monitoria_ativa', $ano_monitoria_ativa);
-$stmt -> bindParam(':semestre_monitoria_ativa', $semestre_monitoria_ativa);
-$stmt -> bindParam(':finaliza_escolhas', 'TRUE');
-$result = $stmt->execute();
 
-            
-            // $query_insere_escolha_aluno = "INSERT INTO escolhas_candidatos ($campos) VALUES($bind_valores)";
-            
-            // $stmt = $PDO->prepare( $query_insere_escolha_aluno );
-            // $stmt -> bindParam(':id_candidato', $id_candidato);
-            // $stmt -> bindParam(':escolha_aluno', $disciplinas_escolhidas['escolha_aluno_'.$i]);
-            // $stmt -> bindParam(':mencao_aluno', $disciplinas_escolhidas['mencao_aluno_'.$i]);
-            // $stmt -> bindParam(':ano_cursado', $disciplinas_escolhidas['ano_cursado_'.$i]);
-            // $stmt -> bindParam(':semestre_cursado', $disciplinas_escolhidas['semestre_cursado_'.$i]);
-            // $stmt -> bindParam(':ano_monitoria_ativa', $ano_monitoria_ativa);
-            // $stmt -> bindParam(':semestre_monitoria_ativa', $semestre_monitoria_ativa);
-            // $result = $stmt->execute();
+
      
 
  //Usar para processar o cadastro do candidato. 
--- funcoes/users.php
+++ funcoes/users.php
@@ -1,21 +1,43 @@
 <?php
-function grava_escolhas_monitor($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas){
+function grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas){
     
     GLOBAL $PDO;
     GLOBAL $numero_escolhas_possiveis;
 
-    $campos = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, tipomonitoria, nome_hora_monitoria, concordatermos';
+    $campos = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, ano_monitoria_ativa, semestre_monitoria_ativa';
+
+    $bind_valores = ':id_candidato, :escolha_aluno, :mencao_aluno, :ano_cursado, :semestre_cursado, :ano_monitoria_ativa, :semestre_monitoria_ativa';
+
+    $ano_monitoria_ativa = $ano_monitoria;
+    $semestre_monitoria_ativa = $semestre_monitoria;
+    $escolheu_hora = horarios_escolhidos_candidato($disciplinas_escolhidas);
+
+    $gravou = array();
 
     for ($i=0; $i < $numero_escolhas_possiveis; $i++) { 
         if ($disciplinas_escolhidas['escolha_aluno_'.$i] !== 'disciplina_vazia') {
+            
             $query_insere_escolha_aluno = "INSERT INTO escolhas_candidatos ($campos) VALUES($bind_valores)";
+            
             $stmt = $PDO->prepare( $query_insere_escolha_aluno );
+            $stmt -> bindParam(':id_candidato', $id_candidato);
+            $stmt -> bindParam(':escolha_aluno', $disciplinas_escolhidas['escolha_aluno_'.$i]);
+            $stmt -> bindParam(':mencao_aluno', $disciplinas_escolhidas['mencao_aluno_'.$i]);
+            $stmt -> bindParam(':ano_cursado', $disciplinas_escolhidas['ano_cursado_'.$i]);
+            $stmt -> bindParam(':semestre_cursado', $disciplinas_escolhidas['semestre_cursado_'.$i]);
+            $stmt -> bindParam(':ano_monitoria_ativa', $ano_monitoria_ativa);
+            $stmt -> bindParam(':semestre_monitoria_ativa', $semestre_monitoria_ativa);
             $result = $stmt->execute();
+
+            if ($result) {
+                $gravou[] = 1;
+            }else{
+                $gravou[] = 0;
+            }
         }
     }
 
-
-
+    return $gravou;
 }
 
 ?>
