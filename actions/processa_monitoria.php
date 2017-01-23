2ab434240923ba416c165450945429dd8d096441
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-23 14:07:27 -0200

    Variável com o número de períodos de monitoria.

--- actions/processa_monitoria.php
+++ actions/processa_monitoria.php
@@ -5,6 +5,17 @@ $disciplinas_escolhidas = $_POST;
 var_dump($disciplinas_escolhidas);
 
 
+for ($i=0; $i < $numero_escolhas_possiveis; $i++) { 
+    if ($disciplinas_escolhidas['escolha_aluno_'.$i] !== 'disciplina_vazia' AND $disciplinas_escolhidas['mencao_aluno_'.$i] === 'mencao_vazia') {
+        $errors[] = "Você não selecionou a Menção que obteve na disciplina ".$disciplinas_escolhidas['escolha_aluno_'.$i].".";
+    }else if ($disciplinas_escolhidas['escolha_aluno_'.$i] !== 'disciplina_vazia' AND $disciplinas_escolhidas['ano_cursado_'.$i] === 'ano_vazio') {
+        $errors[] = "Você não selecionou o Ano que cursou disciplina ".$disciplinas_escolhidas['escolha_aluno_'.$i].".";
+    }else if ($disciplinas_escolhidas['escolha_aluno_'.$i] !== 'disciplina_vazia' AND $disciplinas_escolhidas['semestre_cursado_'.$i] === 'semestre_vazio') {
+        $errors[] = "Você não selecionou o Semestre que cursou disciplina ".$disciplinas_escolhidas['escolha_aluno_'.$i].".";
+    }
+}
+
+print_r($errors);
 
 
 
--- config/init.php
+++ config/init.php
@@ -7,5 +7,6 @@ require_once $ROOT_PATH."config/caminhos.php";
 require_once $ROOT_PATH.'db/db_config.php';
 $PDO = conecta();
 $numero_escolhas_possiveis = 4;
+$numero_horarios_possiveis = 3;
 $errors = array();
 ?>
