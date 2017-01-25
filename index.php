1ee70ea5d19c0c7490190170ccaf6af29316a33c
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-25 17:55:16 -0200

    Começo da criação das rotas de acordo com o tipo de usuário.

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 require_once "config/init.php";
 
+if (isset($_SESSION['id_user'])) {
+ echo "aqui";
+}
+
 $tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
 
-    $tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
+$tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
 
-    $tpl = carrega_template_login_registro();
+$tpl = carrega_template_login_registro();
 
-// if (isset($_SESSION['id_user'])) {
-//      echo "aqui";
-    
-//     // $tpl_main -> parse('escolher_disciplina_monitoria');
-    $tpl_main -> setVariable('escolher_disciplina_monitoria',$tpl->get());
-    
-// }
+$tpl_main -> parse('exibe_paginas');
+$tpl_main -> setVariable('exibe_paginas',$tpl->get());
+
 $tpl_main->show();
 
 ?>
