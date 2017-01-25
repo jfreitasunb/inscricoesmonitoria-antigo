db5ec5ffcece1dd37a9e38db75b69cd705299df0
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-25 15:40:05 -0200

    Rotina para ativar a conta.

--- actions/activate.php
+++ actions/activate.php
@@ -5,15 +5,20 @@ if (isset($_GET['email'], $_GET['email_code'])) {
     $email = trim(strtolower($_GET['email']));
     $email_code = trim($_GET['email_code']);
 
-    echo email_exists($email);
+    if (!email_exists($email)) {
+        $errors[] = "O e-mail informado não está cadastrado no sistema.";
+    }else if (!activate($email, $email_code)){
+        $errors[] = "Não foi possível ativar sua conta. Tente mais tarde.";
+    }
 
-    activate($email, $email_code);
-    // if (!email_exists($email)) {
-    //     $errors[] = "O e-mail informado não está cadastrado no sistema.";
-    // }else if (!activate($email, $email_code)){
-    //     $errors[] = "Não foi possível ativar sua conta. Tente mais tarde.";
-    // }
+    if (!empty($errors)) {
+        mostra_errors($errors);
+    }else{
+        header('Location: actions/activate.php?sucesso');
+        exit();
+    }
+}else{
+    header('Location:../index.php');
+    exit();
 }
-// activate($email, $email_code);
-print_r($errors);
 ?>
