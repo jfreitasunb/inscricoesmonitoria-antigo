2e231ddcd5f5403e0b58b47645fe9d4d46bf6f07
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-25 16:31:04 -0200

    Rotina para efetuar o login.

---
+++ actions/login.php
@@ -0,0 +1,35 @@
+<?php
+require_once "../config/init.php";
+
+if (!empty($_POST)) {
+    
+    $errors = valida_usuario_login();
+
+    
+}else{
+    header('Location:../index.php');
+    exit();
+}
+
+// $campos = implode(', ', array_keys($disciplinas_escolhidas));
+// $bind_valores = ':' . implode(', :', array_keys($disciplinas_escolhidas));
+
+// $query_insere_escolha_aluno = "INSERT INTO escolhas_candidatos ($campos) VALUES($bind_valores)";
+
+// $stmt = $PDO->prepare( $query_insere_escolha_aluno );
+
+
+// foreach ($disciplinas_escolhidas as $key => &$value) {
+//     $stmt -> bindParam(':'.$key, $value);   
+// }
+    
+// $result = $stmt->execute();
+
+// if ($result) {
+//     echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+//     header("location:../index.php");
+//     exit();
+// }else{
+//     echo "Houve um problema com a sua solicitação. Por favor tente mais tarde.";
+// }
+?>
