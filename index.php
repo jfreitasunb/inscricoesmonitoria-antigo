ef1e3920f0ef3f47166068df60c3666b070fb1b0
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-26 18:01:32 -0200

    Template com mensagem de sucesso.

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once "config/init.php";
 
-var_dump($_POST);
-
 $tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
 
 $tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
@@ -20,14 +18,34 @@ if (!empty($_POST)) {
             $tpl->setVariable('mensagem_erros', mostra_erros($errors));
             $tpl_main -> parse('exibe_errors');
             $tpl_main -> setVariable('exibe_errors',$tpl->get());
-        }else if (isset($_POST['registrar']) && $_POST['registrar'] === "Registrar"){
-            echo "entrei aqui";
         }
-    }
-    
-    
+    }else if (isset($_POST['registrar']) && $_POST['registrar'] === "Registrar"){
+            $errors = valida_usuario_registrar();
+            if (!empty($errors)) {
+                $tpl = carrega_mensagem_erro();
+                $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                $tpl_main -> parse('exibe_errors');
+                $tpl_main -> setVariable('exibe_errors',$tpl->get());
+            }
 
+                if (empty($errors)) {
+                    $nome = htmlentities($_POST['nome'], ENT_QUOTES, "UTF-8");
+                    $dados_usuario_novo = prepara_dados();
+                    $errors = grava_usuario_novo($dados_usuario_novo);
 
+                    if (empty($errors)) {
+                        $validation_code = $dados_usuario_novo['validation_code'];
+                        $email = $dados_usuario_novo['email'];
+
+                        $resultado = envia_email_ativa_conta($nome,$email,$validation_code);
+
+                        // if ($resultado) {
+                        //     header('Location: processa_nova_conta.php?sucesso');
+                        //     exit();
+                        // }
+                    }
+                }
+        }
 }
 
 $tpl_main->show();
--- templates/success.tpl
+++ templates/success.tpl
@@ -1,8 +1,8 @@
-<div class="bs-example">
-    <div class="alert alert-danger fade in">
+<div class="bs-example col-md-6 col-md-offset-3">
+    <div class="alert alert-success fade in">
         <a href="#" class="close" data-dismiss="alert">&times;</a>
-        <!-- BEGIN mensagem_erros -->
-            {mensagem_erros}
-        <!-- END mensagem_errors -->>
+        <!-- BEGIN mensagem_sucesso -->
+            {mensagem_sucesso}
+        <!-- END mensagem_sucesso -->
     </div>
 </div>
