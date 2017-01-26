c2f159aea2cd458bcb1b773e41beeba2ad967dfa
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-26 17:40:45 -0200

    Processamento da rotina de login ou de registro.

--- index.php
+++ index.php
@@ -13,13 +13,15 @@ $tpl_main -> parse('exibe_paginas');
 $tpl_main -> setVariable('exibe_paginas',$tpl->get());
 
 if (!empty($_POST)) {
-    if ($_POST['login'] === "Entrar") {
+    if (isset($_POST['login']) && $_POST['login'] === "Entrar") {
         $errors = processa_login();
         if (!empty($errors)) {
             $tpl = carrega_mensagem_erro();
             $tpl->setVariable('mensagem_erros', mostra_erros($errors));
             $tpl_main -> parse('exibe_errors');
             $tpl_main -> setVariable('exibe_errors',$tpl->get());
+        }else if (isset($_POST['registrar']) && $_POST['registrar'] === "Registrar"){
+            echo "entrei aqui";
         }
     }
     
