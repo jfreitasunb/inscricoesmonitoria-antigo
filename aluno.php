0fb9bfdddf5630fbfa56d7955cbe846308d9d726
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-27 16:42:16 -0200

    Início da página do aluno.

--- aluno.php
+++ aluno.php
@@ -1,8 +1,14 @@
 <?php
 require_once "config/init.php";
 protect_page();
-echo "aqui";
 
+$tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
 
+$tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
 
+$tpl carrega_menu_aluno();
+
+$tpl_main -> setVariable('exibe_menus',$tpl->get());
+
+$tpl_main->show();
 ?>
