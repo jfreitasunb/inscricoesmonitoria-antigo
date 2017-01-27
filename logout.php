6b49d079ffb2121c297c60158c59284536d46ba7
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-27 18:03:42 -0200

    Formulário de logout.

--- logout.php
+++ logout.php
@@ -1,11 +1,5 @@
 <?php
 require_once "config/init.php";
-
-$tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
-
-$tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
-
-
-$tpl_main->show();
+session_destroy();
 echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=index.php'>";
 ?>
