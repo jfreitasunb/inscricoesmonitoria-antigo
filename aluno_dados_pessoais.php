d53756e5ea7c68f1f6943043145986ea67b6b2a8
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-27 16:49:36 -0200

    Apresentar o menu junto com o formulário de dados pessoais.

--- aluno_dados_pessoais.php
+++ aluno_dados_pessoais.php
@@ -6,9 +6,14 @@ $tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
 
 $tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
 
-$tpl = carrega_dados_pessoais_aluno();
+$tpl_menu = carrega_menu_aluno();
 
-$tpl_main -> setVariable('exibe_paginas',$tpl->get());
+$tpl_main -> setVariable('exibe_menus',$tpl_menu->get());
+
+
+$tpl_dados_pessoais = carrega_dados_pessoais_aluno();
+
+$tpl_main -> setVariable('exibe_paginas',$tpl_dados_pessoais->get());
 
 $tpl_main->show();
 ?>
