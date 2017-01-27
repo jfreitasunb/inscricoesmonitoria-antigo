ecf743b49de6fddb7b3319764ecb8ea55f405027
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-27 16:43:46 -0200

    Carregamento da página do aluno.

--- aluno.php
+++ aluno.php
@@ -6,7 +6,7 @@ $tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
 
 $tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
 
-$tpl carrega_menu_aluno();
+$tpl = carrega_menu_aluno();
 
 $tpl_main -> setVariable('exibe_menus',$tpl->get());
 
