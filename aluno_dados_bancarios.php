9760f91941b1e8f8cf76d4dd6c13243ff24a8f37
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-01-30 17:27:30 -0200

    Template para informação dos dados bancários.

---
+++ aluno_dados_bancarios.php
@@ -0,0 +1,41 @@
+<?php
+require_once "config/init.php";
+protect_page();
+
+$tpl_main = new HTML_Template_Sigma($PATH_TEMPLATES);
+
+$tpl_main->loadTemplatefile("cabecalho_rodape.tpl");
+
+$tpl_menu = carrega_menu_aluno();
+
+$tpl_main -> setVariable('exibe_menus',$tpl_menu->get());
+
+
+$tpl_dados_pessoais = carrega_dados_pessoais_aluno();
+
+$tpl_main -> setVariable('exibe_paginas',$tpl_dados_pessoais->get());
+
+$tpl_main->show();
+
+
+$errors = valida_dados_pessoais();
+
+$dados_pessoais  = array(
+    'nome'      => $_POST['nome'],
+    'numerorg'  => $_POST['numerorg'], 
+    'emissorrg' => $_POST['emissorrg'], 
+    'cpf'       => $_POST['cpf'], 
+    'endereco'  => $_POST['endereco'], 
+    'cidade'    => $_POST['cidade'], 
+    'cep'       => $_POST['cep'], 
+    'estado'    => $_POST['estado'], 
+    'telefone'  => $_POST['telefone'],
+    'celular'   => $_POST['celular']
+    );
+
+$dados_pessoais_sanitizados = sanitiza_dados_pessoais($dados_pessoais);
+
+$tabela = 'dados_pessoais';
+
+grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_pessoais_sanitizados,$tabela);
+?>
--- templates/dados_bancarios.tpl
+++ templates/dados_bancarios.tpl
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/css_monitoria.css" rel="stylesheet">
-  
-</head>
-<body>
-<form class="form-horizontal">
+<form class="form-horizontal" action="" method="post">
 <fieldset>
 
 <!-- Form Name -->
@@ -60,11 +49,3 @@
 
 </fieldset>
 </form>
-
-</body>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="../jquery/jquery-3.1.1.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/monitoria.js"></script>
-</html>
