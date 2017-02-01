8fd96fae6376b0a42cb392b0d8e56ca7cfb2cdfe
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-02-01 17:13:41 -0200

    Correção do nome da variável com o id do usuário.

--- aluno_escolher_monitoria.php
+++ aluno_escolher_monitoria.php
@@ -26,9 +26,9 @@ if (autoriza_inscricao()) {
                 $tpl_main -> parse('exibe_mensagens');
                 $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
             }else{
-                $id_candidato = $_SESSION['id_user'];
+                $id_user = $_SESSION['id_user'];
                 $id_monitoria = $_SESSION['id_monitoria'];
-                $resultado = grava_escolhas_monitoria($id_candidato, $id_monitoria,$disciplinas_escolhidas);
+                $resultado = grava_escolhas_monitoria($id_user, $id_monitoria,$disciplinas_escolhidas);
 
                 if ($resultado) {
                         $tpl = carrega_mensagem_sucesso();
@@ -36,7 +36,7 @@ if (autoriza_inscricao()) {
                         $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
                         $tpl_main -> parse('exibe_mensagens');
                         $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-                        $errors = upload_historico($id_candidato);
+                        $errors = upload_historico($id_user);
 
                         if (!empty($errors)) {
                             $tpl = carrega_mensagem_erro();
@@ -44,7 +44,7 @@ if (autoriza_inscricao()) {
                             $tpl_main -> parse('exibe_mensagens');
                             $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
                         }else{
-                            $errors = finaliza_escolhas($id_candidato,$id_monitoria,$disciplinas_escolhidas);
+                            $errors = finaliza_escolhas($id_user,$id_monitoria,$disciplinas_escolhidas);
                         }
                     }else{
                         $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
