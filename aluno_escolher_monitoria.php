cc1e2537a66c9a13290e939905e315a2ca62e538
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-02-01 16:54:32 -0200

    Bloquear inscrição se fora do período.

--- aluno_escolher_monitoria.php
+++ aluno_escolher_monitoria.php
@@ -10,50 +10,59 @@ $tpl_menu = carrega_menu_aluno();
 
 $tpl_main -> setVariable('exibe_menus',$tpl_menu->get());
 
-$tpl_dados_monitoria = preenche_template_monitoria();
+if (autoriza_inscricao()) {
+    $tpl_dados_monitoria = preenche_template_monitoria();
 
-$tpl_main -> setVariable('exibe_paginas',$tpl_dados_monitoria->get());
+    $tpl_main -> setVariable('exibe_paginas',$tpl_dados_monitoria->get());
 
+    if (!empty($_POST)) {
+        $disciplinas_escolhidas = $_POST;
+        $errors = valida_escolhas_aluno($disciplinas_escolhidas);
 
-if (!empty($_POST)) {
-    $disciplinas_escolhidas = $_POST;
-    $errors = valida_escolhas_aluno($disciplinas_escolhidas);
+        if (!empty($errors)) {
+            $tpl = carrega_mensagem_erro();
+            $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+            $tpl_main -> parse('exibe_mensagens');
+            $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+        }else{
+            $id_candidato = $_SESSION['id_user'];
+            $id_monitoria = $_SESSION['id_monitoria'];
+            $resultado = grava_escolhas_monitoria($id_candidato, $id_monitoria,$disciplinas_escolhidas);
 
-    if (!empty($errors)) {
-        $tpl = carrega_mensagem_erro();
-        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-        $tpl_main -> parse('exibe_mensagens');
-        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-    }else{
-        $id_candidato = $_SESSION['id_user'];
-        $id_monitoria = $_SESSION['id_monitoria'];
-        $resultado = grava_escolhas_monitoria($id_candidato, $id_monitoria,$disciplinas_escolhidas);
+            if ($resultado) {
+                    $tpl = carrega_mensagem_sucesso();
+                    $mensagem_sucesso = "Seus escolhas para monitoria foram gravadas em nosso sitemas.";
+                    $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
+                    $tpl_main -> parse('exibe_mensagens');
+                    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                    $errors = upload_historico($id_candidato);
 
-        if ($resultado) {
-                $tpl = carrega_mensagem_sucesso();
-                $mensagem_sucesso = "Seus escolhas para monitoria foram gravadas em nosso sitemas.";
-                $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
-                $tpl_main -> parse('exibe_mensagens');
-                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-                $errors = upload_historico($id_candidato);
-
-                if (!empty($errors)) {
+                    if (!empty($errors)) {
+                        $tpl = carrega_mensagem_erro();
+                        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                        $tpl_main -> parse('exibe_mensagens');
+                        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                    }else{
+                        $errors = finaliza_escolhas($id_candidato,$id_monitoria,$disciplinas_escolhidas);
+                    }
+                }else{
+                    $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
                     $tpl = carrega_mensagem_erro();
                     $tpl->setVariable('mensagem_erros', mostra_erros($errors));
                     $tpl_main -> parse('exibe_mensagens');
                     $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-                }else{
-                    $errors = finaliza_escolhas($id_candidato,$id_monitoria,$disciplinas_escolhidas);
                 }
-            }else{
-                $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
-                $tpl = carrega_mensagem_erro();
-                $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-                $tpl_main -> parse('exibe_mensagens');
-                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-            }
+        }
     }
+}else{
+    $errors[] = "O período de inscrição para ser monitor do MAT já se encerrou. Tente novamente no próximo semestre.";
+    $tpl = carrega_mensagem_erro();
+    $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+    $tpl_main -> parse('exibe_mensagens');
+    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
 }
+
+
 
 $tpl_main->show();
 ?>
