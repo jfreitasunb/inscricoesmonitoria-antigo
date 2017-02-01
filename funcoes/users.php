4730dad66d81324801c070b5548502698b60019a
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-02-01 15:45:05 -0200

    Mudança na função grava_escolhas_monitoria. Remoção de valores desnecessários.

--- funcoes/users.php
+++ funcoes/users.php
@@ -476,20 +476,18 @@ function upload_historico($id_candidato){
     }
 }
 
-function grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas){
+function grava_escolhas_monitoria($id_candidato, $id_monitoria,$disciplinas_escolhidas){
     
     GLOBAL $PDO;
     GLOBAL $numero_escolhas_possiveis;
 
-    $campos = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, ano_monitoria_ativa, semestre_monitoria_ativa';
+    $campos = 'id_candidato, escolha_aluno, mencao_aluno, ano_cursado, semestre_cursado, id_monitoria';
 
-    $bind_valores = ':id_candidato, :escolha_aluno, :mencao_aluno, :ano_cursado, :semestre_cursado, :ano_monitoria_ativa, :semestre_monitoria_ativa';
+    $bind_valores = ':id_candidato, :escolha_aluno, :mencao_aluno, :ano_cursado, :semestre_cursado, :id_monitoria';
 
-    $ano_monitoria_ativa = $ano_monitoria;
-    $semestre_monitoria_ativa = $semestre_monitoria;
+
     $escolheu_hora = horarios_escolhidos_candidato($disciplinas_escolhidas);
 
-    $gravou = array();
 
     for ($i=0; $i < $numero_escolhas_possiveis; $i++) { 
         if ($disciplinas_escolhidas['escolha_aluno_'.$i] !== 'disciplina_vazia') {
@@ -502,8 +500,7 @@ function grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monit
             $stmt -> bindParam(':mencao_aluno', $disciplinas_escolhidas['mencao_aluno_'.$i]);
             $stmt -> bindParam(':ano_cursado', $disciplinas_escolhidas['ano_cursado_'.$i]);
             $stmt -> bindParam(':semestre_cursado', $disciplinas_escolhidas['semestre_cursado_'.$i]);
-            $stmt -> bindParam(':ano_monitoria_ativa', $ano_monitoria_ativa);
-            $stmt -> bindParam(':semestre_monitoria_ativa', $semestre_monitoria_ativa);
+            $stmt -> bindParam(':id_monitoria', $id_monitoria);
             $result = $stmt->execute();
 
             if ($result) {
@@ -513,8 +510,6 @@ function grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monit
             }
         }
     }
-
-    return $gravou;
 }
 
 function finaliza_escolhas($id_candidato, $id_monitoria,$disciplinas_escolhidas){
