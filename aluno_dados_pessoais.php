0aa12cf8a0bab60b83cea7303cfeb94955aab912
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-02-01 15:03:01 -0200

    Exibe mensagens de erro caso existam.

--- aluno_dados_pessoais.php
+++ aluno_dados_pessoais.php
@@ -15,27 +15,36 @@ $tpl_dados_pessoais = carrega_dados_pessoais_aluno();
 
 $tpl_main -> setVariable('exibe_paginas',$tpl_dados_pessoais->get());
 
+
+
+
+if (!empty($_POST)) {
+    $errors = valida_dados_pessoais();
+    if (!empty($errors)) {    
+        $tpl = carrega_mensagem_erro();
+        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+        $tpl_main -> parse('exibe_mensagens');
+        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+        }
+}
+
 $tpl_main->show();
+// $dados_pessoais  = array(
+//     'nome'      => $_POST['nome'],
+//     'numerorg'  => $_POST['numerorg'], 
+//     'emissorrg' => $_POST['emissorrg'], 
+//     'cpf'       => $_POST['cpf'], 
+//     'endereco'  => $_POST['endereco'], 
+//     'cidade'    => $_POST['cidade'], 
+//     'cep'       => $_POST['cep'], 
+//     'estado'    => $_POST['estado'], 
+//     'telefone'  => $_POST['telefone'],
+//     'celular'   => $_POST['celular']
+//     );
 
+// $dados_pessoais_sanitizados = sanitiza_dados_pessoais($dados_pessoais);
 
-$errors = valida_dados_pessoais();
+// $tabela = 'dados_pessoais';
 
-$dados_pessoais  = array(
-    'nome'      => $_POST['nome'],
-    'numerorg'  => $_POST['numerorg'], 
-    'emissorrg' => $_POST['emissorrg'], 
-    'cpf'       => $_POST['cpf'], 
-    'endereco'  => $_POST['endereco'], 
-    'cidade'    => $_POST['cidade'], 
-    'cep'       => $_POST['cep'], 
-    'estado'    => $_POST['estado'], 
-    'telefone'  => $_POST['telefone'],
-    'celular'   => $_POST['celular']
-    );
-
-$dados_pessoais_sanitizados = sanitiza_dados_pessoais($dados_pessoais);
-
-$tabela = 'dados_pessoais';
-
-grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_pessoais_sanitizados,$tabela);
+// grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_pessoais_sanitizados,$tabela);
 ?>
