2b1e64a52be70012b560f9228c066af5645818d0
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-02-01 15:40:39 -0200

    Processamento das escolhas do candidato.

--- aluno_escolher_monitoria.php
+++ aluno_escolher_monitoria.php
@@ -15,26 +15,49 @@ $tpl_dados_monitoria = preenche_template_monitoria();
 
 $tpl_main -> setVariable('exibe_paginas',$tpl_dados_monitoria->get());
 
-$tpl_main->show();
 
 
-
-$disciplinas_escolhidas = $_POST;
-$errors = valida_escolhas_aluno($disciplinas_escolhidas);
-$id_candidato = $_SESSION['id_user'];
-$ano_monitoria = '2017';
-$semestre_monitoria = '1';
-
-if (empty($errors)){
-    $resultado = grava_escolhas_monitoria($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas);
+if (!empty($_POST)) {
+    $disciplinas_escolhidas = $_POST;
+    $errors = valida_escolhas_aluno($disciplinas_escolhidas);
     
-    if (in_array(0, $resultado)) {
-        $errors[] = "Houve um erro na hora de gravar suas escolhas. Tente novamente mais tarde.";
+
+    if (!empty($errors)) {
+        $tpl = carrega_mensagem_erro();
+        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+        $tpl_main -> parse('exibe_mensagens');
+        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
     }else{
-        $errors = upload_historico($id_candidato);
-        if (empty($errors)) {
-            $errors = finaliza_escolhas($id_candidato, $ano_monitoria, $semestre_monitoria,$disciplinas_escolhidas);
-        }
+        $id_candidato = $_SESSION['id_user'];
+        $id_monitoria_ativa = $_SESSION['id_monitoria_ativa'];
+        $resultado = grava_escolhas_monitoria($id_candidato, $id_monitoria_ativa,$disciplinas_escolhidas);
+
+        if ($resultado) {
+                $tpl = carrega_mensagem_sucesso();
+                $mensagem_sucesso = "Seus escolhas para monitoria foram gravadas em nosso sitemas.";
+                $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
+                $tpl_main -> parse('exibe_mensagens');
+                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                $errors = upload_historico($id_candidato);
+
+                if (!empty($errors)) {
+                    $tpl = carrega_mensagem_erro();
+                    $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                    $tpl_main -> parse('exibe_mensagens');
+                    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                }else{
+                    $errors = finaliza_escolhas($id_candidato,$id_monitoria_ativa,$disciplinas_escolhidas);
+                }
+
+            }else{
+                $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
+                $tpl = carrega_mensagem_erro();
+                $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                $tpl_main -> parse('exibe_mensagens');
+                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+            }
     }
 }
+
+$tpl_main->show();
 ?>
