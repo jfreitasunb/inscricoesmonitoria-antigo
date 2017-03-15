f86a20f120d665cb78795b5cab39e24459bf09e4
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-03-15 14:59:02 -0300

    Gravação dos dados acadêmicos no BD.

--- aluno_dados_academicos.php
+++ aluno_dados_academicos.php
@@ -26,28 +26,28 @@ if (!empty($_POST)) {
         $tpl_main -> parse('exibe_mensagens');
         $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
     }else{
-        $dados_bancarios_sanitizados = sanitiza_dados($dados_bancarios);
+        $dados_academicos_sanitizados = sanitiza_dados($_POST);
 
-    //     $tabela = "dados_bancarios";
+        $tabela = "dados_academicos";
 
-    //     $resultado = grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_bancarios_sanitizados,$tabela);
+        $resultado = grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_academicos_sanitizados,$tabela);
 
-    //     if ($resultado) {
-    //             $tpl = carrega_mensagem_sucesso();
-    //             $mensagem_sucesso = "Seus dados bancários foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
-    //             $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
-    //             $tpl_main -> parse('exibe_mensagens');
-    //             $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-    //             $http = new HTTP2();
-    //             $http->redirect("aluno_escolher_monitoria.php");
-    //             echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
-    //         }else{
-    //             $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
-    //             $tpl = carrega_mensagem_erro();
-    //             $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-    //             $tpl_main -> parse('exibe_mensagens');
-    //             $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-    //         }
+        if ($resultado) {
+                $tpl = carrega_mensagem_sucesso();
+                $mensagem_sucesso = "Seus dados acadêmicos foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
+                $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
+                $tpl_main -> parse('exibe_mensagens');
+                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                $http = new HTTP2();
+                $http->redirect("aluno_escolher_monitoria.php");
+                echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+            }else{
+                $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
+                $tpl = carrega_mensagem_erro();
+                $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                $tpl_main -> parse('exibe_mensagens');
+                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+            }
     }
 }
 
