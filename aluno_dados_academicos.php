b75a693f3a8423096f0c692ca38b1a10f6a6b7bb
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-03-15 17:08:22 -0300

    Inclusão do id da monitoria.

--- aluno_dados_academicos.php
+++ aluno_dados_academicos.php
@@ -15,6 +15,17 @@ $tpl_dados_bancarios = carrega_template_dados_academicos();
 
 $tpl_main -> setVariable('exibe_paginas',$tpl_dados_bancarios->get());
 
+$id_user = $_SESSION['id_user'];
+$id_monitoria = $_SESSION['id_monitoria'];
+
+
+$dados_academicos = array(
+    'ira'               => $_POST['ira'],
+    'curso_graduacao'   => $_POST['curso_graduacao']);
+// var_dump($_POST);
+// 
+print_r($_SESSION);
+
 
 if (!empty($_POST)) {
     
@@ -26,11 +37,11 @@ if (!empty($_POST)) {
         $tpl_main -> parse('exibe_mensagens');
         $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
     }else{
-        $dados_academicos_sanitizados = sanitiza_dados($_POST);
+        $dados_academicos_sanitizados = sanitiza_dados($dados_academicos);
 
         $tabela = "dados_academicos";
 
-        $resultado = grava_dados_pessoais_usuario($_SESSION['id_user'],$dados_academicos_sanitizados,$tabela);
+        $resultado = grava_dados_pessoais_usuario($id_user,$id_monitoria,$dados_academicos_sanitizados,$tabela);
 
         if ($resultado) {
                 $tpl = carrega_mensagem_sucesso();
@@ -49,7 +60,7 @@ if (!empty($_POST)) {
                 }else{
                     // $http = new HTTP2();
                     // $http->redirect("aluno_escolher_monitoria.php");
-                    echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+                    // echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
                 }
             }else{
                 $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
