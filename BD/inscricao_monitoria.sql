44126473ecec04d55901ec2e5eeee8e3b1de948a
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-03-15 17:40:13 -0300

    Mudança nas tabelas.

--- BD/inscricao_monitoria.sql
+++ BD/inscricao_monitoria.sql
@@ -47,7 +47,7 @@ ALTER TABLE public.arquivos_enviados OWNER TO monitoria;
 --
 
 CREATE TABLE atuou_monitoria (
-    id_atuacao integer NOT NULL,
+    id_user integer NOT NULL,
     atuou_disciplina character varying(200) NOT NULL
 );
 
@@ -72,7 +72,7 @@ ALTER TABLE public.atuou_monitoria_id_atuacao_seq OWNER TO monitoria;
 -- Name: atuou_monitoria_id_atuacao_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: monitoria
 --
 
-ALTER SEQUENCE atuou_monitoria_id_atuacao_seq OWNED BY atuou_monitoria.id_atuacao;
+ALTER SEQUENCE atuou_monitoria_id_atuacao_seq OWNED BY atuou_monitoria.id_user;
 
 
 --
@@ -150,11 +150,11 @@ ALTER SEQUENCE cursos_graduacao_id_curso_graduacao_seq OWNED BY cursos_graduacao
 
 CREATE TABLE dados_academicos (
     id_user integer NOT NULL,
-    ira double precision NOT NULL,
-    foi_monitor character varying(50) NOT NULL,
-    monitor_convidado character varying(1) NOT NULL,
+    ira double precision,
+    monitor_convidado character varying(1),
     nome_professor character varying(200),
-    curso_graduacao integer NOT NULL
+    curso_graduacao character varying(100),
+    id_monitoria integer NOT NULL
 );
 
 
@@ -349,10 +349,10 @@ ALTER SEQUENCE users_id_user_seq OWNED BY users.id_user;
 
 
 --
--- Name: id_atuacao; Type: DEFAULT; Schema: public; Owner: monitoria
+-- Name: id_user; Type: DEFAULT; Schema: public; Owner: monitoria
 --
 
-ALTER TABLE ONLY atuou_monitoria ALTER COLUMN id_atuacao SET DEFAULT nextval('atuou_monitoria_id_atuacao_seq'::regclass);
+ALTER TABLE ONLY atuou_monitoria ALTER COLUMN id_user SET DEFAULT nextval('atuou_monitoria_id_atuacao_seq'::regclass);
 
 
 --
@@ -402,7 +402,7 @@ COPY arquivos_enviados (id_user, nome_arquivo, data_envio) FROM stdin;
 -- Data for Name: atuou_monitoria; Type: TABLE DATA; Schema: public; Owner: monitoria
 --
 
-COPY atuou_monitoria (id_atuacao, atuou_disciplina) FROM stdin;
+COPY atuou_monitoria (id_user, atuou_disciplina) FROM stdin;
 \.
 
 
@@ -418,6 +418,7 @@ SELECT pg_catalog.setval('atuou_monitoria_id_atuacao_seq', 1, false);
 --
 
 COPY configura_monitoria (id_monitoria, ano_monitoria, semestre_monitoria, inicio_inscricao, fim_inscricao) FROM stdin;
+2	2017	1	2017-03-15	2017-06-30
 \.
 
 
@@ -425,7 +426,7 @@ COPY configura_monitoria (id_monitoria, ano_monitoria, semestre_monitoria, inici
 -- Name: configura_monitoria_id_monitoria_seq; Type: SEQUENCE SET; Schema: public; Owner: monitoria
 --
 
-SELECT pg_catalog.setval('configura_monitoria_id_monitoria_seq', 1, true);
+SELECT pg_catalog.setval('configura_monitoria_id_monitoria_seq', 2, true);
 
 
 --
@@ -433,14 +434,14 @@ SELECT pg_catalog.setval('configura_monitoria_id_monitoria_seq', 1, true);
 --
 
 COPY cursos_graduacao (id_curso_graduacao, nome_curso) FROM stdin;
-1   Matemática (Bacharelado/Licenciatura)
-2   Ciências da Computação (Bacharelado/Licenciatura)
-3   Estatística
-4   Física (Bacharelado/Licenciatura)
-5   Química (Bacharelado/Licenciatura)
-6   Geologia/Geofísica
-7   Engenharia (Mecânica/Elétrica/Civil/Redes/Mecatrônica/Química/Produção)
-8   Outros
+1	Matemática (Bacharelado/Licenciatura)
+2	Ciências da Computação (Bacharelado/Licenciatura)
+3	Estatística
+4	Física (Bacharelado/Licenciatura)
+5	Química (Bacharelado/Licenciatura)
+6	Geologia/Geofísica
+7	Engenharia (Mecânica/Elétrica/Civil/Redes/Mecatrônica/Química/Produção)
+8	Outros
 \.
 
 
@@ -455,7 +456,8 @@ SELECT pg_catalog.setval('cursos_graduacao_id_curso_graduacao_seq', 8, true);
 -- Data for Name: dados_academicos; Type: TABLE DATA; Schema: public; Owner: monitoria
 --
 
-COPY dados_academicos (id_user, ira, foi_monitor, monitor_convidado, nome_professor, curso_graduacao) FROM stdin;
+COPY dados_academicos (id_user, ira, monitor_convidado, nome_professor, curso_graduacao, id_monitoria) FROM stdin;
+7	3	\N	\N	geologia_geofisica	2
 \.
 
 
@@ -471,6 +473,7 @@ SELECT pg_catalog.setval('dados_academicos_id_aluno_seq', 1, false);
 --
 
 COPY dados_bancarios (id_user, nomebanco, numerobanco, agenciabancaria, numerocontacorrente) FROM stdin;
+7	\N	\N	\N	\N
 \.
 
 
@@ -479,6 +482,7 @@ COPY dados_bancarios (id_user, nomebanco, numerobanco, agenciabancaria, numeroco
 --
 
 COPY dados_pessoais (id_user, nome, numerorg, emissorrg, cpf, endereco, cidade, cep, estado, telefone, celular) FROM stdin;
+7	Eu	\N	\N	\N	\N	\N	\N	\N	\N	\N
 \.
 
 
@@ -487,6 +491,50 @@ COPY dados_pessoais (id_user, nome, numerorg, emissorrg, cpf, endereco, cidade, 
 --
 
 COPY disciplinas_disponiveis (id, id_monitoria, codigo_disciplina) FROM stdin;
+1	2	105881
+2	2	113018
+3	2	113026
+4	2	113034
+5	2	113042
+6	2	113051
+7	2	113069
+8	2	113093
+9	2	113107
+10	2	113115
+11	2	113123
+12	2	113131
+13	2	113204
+14	2	113212
+15	2	113263
+16	2	113301
+17	2	113328
+18	2	113417
+19	2	113506
+20	2	113522
+21	2	113611
+22	2	113701
+23	2	113808
+24	2	113824
+25	2	113832
+26	2	113859
+27	2	113972
+28	2	117102
+29	2	117137
+30	2	117145
+31	2	117161
+32	2	117170
+33	2	117323
+34	2	117358
+35	2	117412
+36	2	117421
+37	2	117439
+38	2	117471
+39	2	117480
+40	2	117501
+41	2	117510
+42	2	200107
+43	2	200107
+44	2	200107
 \.
 
 
@@ -494,7 +542,7 @@ COPY disciplinas_disponiveis (id, id_monitoria, codigo_disciplina) FROM stdin;
 -- Name: disciplinas_disponivies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: monitoria
 --
 
-SELECT pg_catalog.setval('disciplinas_disponivies_id_seq', 1, false);
+SELECT pg_catalog.setval('disciplinas_disponivies_id_seq', 44, true);
 
 
 --
@@ -502,48 +550,48 @@ SELECT pg_catalog.setval('disciplinas_disponivies_id_seq', 1, false);
 --
 
 COPY disciplinas_mat (codigo, nome, creditos, status) FROM stdin;
-113115  Teoria dos Números  4   grad
-117323  Teoria dos Números 2    4   grad
-113263  Topologia dos Espaços Métricos  4   grad
-117145  Álgebra 3   4   grad
-113123  Álgebra Linear  6   grad
-113611  Álgebra para Ensino 1 e 2   6   grad
-113212  Análise 2   4   grad
-117137  Análise 3   4   grad
-113972  Análise Combinatória    4   grad
-113859  Análise de Algorítmos   4   grad
-113506  Análise Numérica 1  4   grad
-113034  Cálculo 1   6   grad
-113042  Cálculo 2   6   grad
-113051  Cálculo 3   6   grad
-113824  Cálculo de Probabilidade 1  6   grad
-113832  Cálculo de Probabilidade 2  4   grad
-113417  Cálculo Numérico    4   grad
-113301  Equações Diferenciais 1 4   grad
-113808  Fundamentos de Matemática 1 4   grad
-117161  Geometria 1 4   grad
-117170  Geometria 2 4   grad
-113328  Geometria Diferencial 1 4   grad
-117471  Geometria para o Ensino 1   6   grad
-117480  Geometria para o Ensino 2   6   grad
-113522  Métodos Matemáticos da Física 1 6   grad
-113069  Variável Complexa 1 6   grad
-117421  Álgebra para o Ensino 1 6   grad
-117501  Álgebra para o Ensino 2 6   grad
-117412  Introdução à Teoria da Metida e Integração  4   grad
-113093  Introdução à Álgebra Linear 4   grad
-117358  Lógica Matemática e Computacional   4   grad
-117102  Métodos Matemáticos da Física 2 4   grad
-113107  Álgebra 1   4   grad
-113131  Álgebra 2   4   grad
-113204  Análise 1   4   grad
-200107  Cálculo 1 Semipresencial    6   grad
-105881  Geometria Analítica para Matemática 4   grad
-113701  Introdução à Matemática Superior    6   grad
-113018  Matemática 1    4   grad
-113026  Matemática 2    4   grad
-117510  Regência 1  8   grad
-117439  Regência 2  8   grad
+113115	Teoria dos Números	4	grad
+117323	Teoria dos Números 2	4	grad
+113263	Topologia dos Espaços Métricos	4	grad
+117145	Álgebra 3	4	grad
+113123	Álgebra Linear	6	grad
+113611	Álgebra para Ensino 1 e 2	6	grad
+113212	Análise 2	4	grad
+117137	Análise 3	4	grad
+113972	Análise Combinatória	4	grad
+113859	Análise de Algorítmos	4	grad
+113506	Análise Numérica 1	4	grad
+113034	Cálculo 1	6	grad
+113042	Cálculo 2	6	grad
+113051	Cálculo 3	6	grad
+113824	Cálculo de Probabilidade 1	6	grad
+113832	Cálculo de Probabilidade 2	4	grad
+113417	Cálculo Numérico	4	grad
+113301	Equações Diferenciais 1	4	grad
+113808	Fundamentos de Matemática 1	4	grad
+117161	Geometria 1	4	grad
+117170	Geometria 2	4	grad
+113328	Geometria Diferencial 1	4	grad
+117471	Geometria para o Ensino 1	6	grad
+117480	Geometria para o Ensino 2	6	grad
+113522	Métodos Matemáticos da Física 1	6	grad
+113069	Variável Complexa 1	6	grad
+117421	Álgebra para o Ensino 1	6	grad
+117501	Álgebra para o Ensino 2	6	grad
+117412	Introdução à Teoria da Metida e Integração	4	grad
+113093	Introdução à Álgebra Linear	4	grad
+117358	Lógica Matemática e Computacional	4	grad
+117102	Métodos Matemáticos da Física 2	4	grad
+113107	Álgebra 1	4	grad
+113131	Álgebra 2	4	grad
+113204	Análise 1	4	grad
+200107	Cálculo 1 Semipresencial	6	grad
+105881	Geometria Analítica para Matemática	4	grad
+113701	Introdução à Matemática Superior	6	grad
+113018	Matemática 1	4	grad
+113026	Matemática 2	4	grad
+117510	Regência 1	8	grad
+117439	Regência 2	8	grad
 \.
 
 
@@ -560,6 +608,7 @@ COPY escolhas_candidatos (id_user, escolha_aluno, mencao_aluno, id_monitoria) FR
 --
 
 COPY finaliza_escolhas (id_user, tipo_monitoria, monitor_projeto, nome_professor, concordatermos, id_monitoria, finaliza_escolhas) FROM stdin;
+7	\N	\N	\N	\N	\N	f
 \.
 
 
@@ -576,7 +625,8 @@ COPY horario_escolhido_monitoria (id_user, horario_monitoria, dia_semana, id_mon
 --
 
 COPY users (id_user, login, password, email, validation_code, user_type, ativo) FROM stdin;
-1   coordgrad   $2y$10$D/mVBW7QvbOCPdPBuaGce.RHlOvpEW.A7kzEDg1NJuVvfUzJLpj5q    coordgrad@mat.unb.br    \N  1   1
+1	coordgrad	$2y$10$D/mVBW7QvbOCPdPBuaGce.RHlOvpEW.A7kzEDg1NJuVvfUzJLpj5q	coordgrad@mat.unb.br	\N	1	1
+7	1	$2y$10$.f7fkozvIbmNoMuvBg4q4.C1kEWsUrwH6fq242aOZAmZpmxVoLxya	1@mail.com	c8feb699e5c7c39f5529539db0858777	0	1
 \.
 
 
@@ -584,7 +634,7 @@ COPY users (id_user, login, password, email, validation_code, user_type, ativo) 
 -- Name: users_id_user_seq; Type: SEQUENCE SET; Schema: public; Owner: monitoria
 --
 
-SELECT pg_catalog.setval('users_id_user_seq', 6, true);
+SELECT pg_catalog.setval('users_id_user_seq', 7, true);
 
 
 --
@@ -592,7 +642,7 @@ SELECT pg_catalog.setval('users_id_user_seq', 6, true);
 --
 
 ALTER TABLE ONLY atuou_monitoria
-    ADD CONSTRAINT atuou_monitoria_pkey PRIMARY KEY (id_atuacao);
+    ADD CONSTRAINT atuou_monitoria_pkey PRIMARY KEY (id_user);
 
 
 --
