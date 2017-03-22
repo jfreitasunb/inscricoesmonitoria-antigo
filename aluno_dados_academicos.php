3936a32b3c69f54dd80bd352ecc8728f08b9b632
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-03-22 17:46:26 -0300

    Remoção de campo desnecessário.

--- aluno_dados_academicos.php
+++ aluno_dados_academicos.php
@@ -45,23 +45,31 @@ if (!empty($_POST)) {
 
                 $errors = grava_atuacao_anterior($id_user,$atuou_monitoria);
 
-                $tpl = carrega_mensagem_sucesso();
-                $mensagem_sucesso = "Seus dados acadêmicos foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
-                $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
-                $tpl_main -> parse('exibe_mensagens');
-                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-
-                $errors = upload_historico($id_user);
-
-               if (!empty($errors)) {
+                if (!empty($errors)) {
                     $tpl = carrega_mensagem_erro();
                     $tpl->setVariable('mensagem_erros', mostra_erros($errors));
                     $tpl_main -> parse('exibe_mensagens');
                     $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
                 }else{
-                    // $http = new HTTP2();
-                    // $http->redirect("aluno_escolher_monitoria.php");
-                    // echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+                    
+                    $errors = upload_historico($id_user);
+                    
+                    if (!empty($errors)) {
+                        $tpl = carrega_mensagem_erro();
+                        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                        $tpl_main -> parse('exibe_mensagens');
+                        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                    }else{
+                        // $http = new HTTP2();
+                        // $http->redirect("aluno_escolher_monitoria.php");
+                        // 
+                        $tpl = carrega_mensagem_sucesso();
+                        $mensagem_sucesso = "Seus dados acadêmicos foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
+                        $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
+                        $tpl_main -> parse('exibe_mensagens');
+                        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                        echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+                    }
                 }
             }else{
                 $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
