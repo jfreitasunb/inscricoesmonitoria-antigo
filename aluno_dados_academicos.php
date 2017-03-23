6daa39bce3a27f0f0470f2c24ee892af4a71b6ec
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-03-22 21:16:28 -0300

    Bloqueio de alterações das informações acadêmicas caso o candidato já tenha finalizado a inscrição.

--- aluno_dados_academicos.php
+++ aluno_dados_academicos.php
@@ -10,75 +10,82 @@ $tpl_menu = carrega_menu_aluno();
 
 $tpl_main -> setVariable('exibe_menus',$tpl_menu->get());
 
-
-$tpl_dados_bancarios = carrega_template_dados_academicos();
-
-$tpl_main -> setVariable('exibe_paginas',$tpl_dados_bancarios->get());
-
-$id_user = $_SESSION['id_user'];
-$id_monitoria = $_SESSION['id_monitoria'];
-
-
-if (!empty($_POST)) {
-
-    $atuou_monitoria = $_POST['checkbox_foi_monitor'];
+if (!inscricao_finalizada()) {
     
-    $dados_academicos = array(
-        'ira'               => $ira = str_replace(',', '.', $_POST['ira']),
-        'curso_graduacao'   => $_POST['curso_graduacao']);
+    $tpl_dados_bancarios = carrega_template_dados_academicos();
 
-    $errors = valida_dados_academicos();
+    $tpl_main -> setVariable('exibe_paginas',$tpl_dados_bancarios->get());
 
-    if (!empty($errors)) {
-        $tpl = carrega_mensagem_erro();
-        $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-        $tpl_main -> parse('exibe_mensagens');
-        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-    }else{
-        $dados_academicos_sanitizados = sanitiza_dados($dados_academicos);
+    $id_user = $_SESSION['id_user'];
+    $id_monitoria = $_SESSION['id_monitoria'];
 
-        $tabela = "dados_academicos";
+    if (!empty($_POST)) {
 
-        $errors = grava_dados_pessoais_usuario($id_user,$id_monitoria,$dados_academicos_sanitizados,$tabela);
+        $atuou_monitoria = $_POST['checkbox_foi_monitor'];
+        
+        $dados_academicos = array(
+            'ira'               => $ira = str_replace(',', '.', $_POST['ira']),
+            'curso_graduacao'   => $_POST['curso_graduacao']);
 
-        if (empty($errors)) {
+        $errors = valida_dados_academicos();
 
-                $errors = grava_atuacao_anterior($id_user,$atuou_monitoria);
+        if (!empty($errors)) {
+            $tpl = carrega_mensagem_erro();
+            $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+            $tpl_main -> parse('exibe_mensagens');
+            $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+        }else{
+            $dados_academicos_sanitizados = sanitiza_dados($dados_academicos);
 
-                if (!empty($errors)) {
-                    $tpl = carrega_mensagem_erro();
-                    $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-                    $tpl_main -> parse('exibe_mensagens');
-                    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-                }else{
-                    
-                    $errors = upload_historico($id_user);
-                    
+            $tabela = "dados_academicos";
+
+            $errors = grava_dados_pessoais_usuario($id_user,$id_monitoria,$dados_academicos_sanitizados,$tabela);
+
+            if (empty($errors)) {
+
+                    $errors = grava_atuacao_anterior($id_user,$atuou_monitoria);
+
                     if (!empty($errors)) {
                         $tpl = carrega_mensagem_erro();
                         $tpl->setVariable('mensagem_erros', mostra_erros($errors));
                         $tpl_main -> parse('exibe_mensagens');
                         $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
                     }else{
-                        // $http = new HTTP2();
-                        // $http->redirect("aluno_escolher_monitoria.php");
-                        // 
-                        $tpl = carrega_mensagem_sucesso();
-                        $mensagem_sucesso = "Seus dados acadêmicos foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
-                        $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
-                        $tpl_main -> parse('exibe_mensagens');
-                        $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-                        echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+                        
+                        $errors = upload_historico($id_user);
+                        
+                        if (!empty($errors)) {
+                            $tpl = carrega_mensagem_erro();
+                            $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                            $tpl_main -> parse('exibe_mensagens');
+                            $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                        }else{
+                            // $http = new HTTP2();
+                            // $http->redirect("aluno_escolher_monitoria.php");
+                            // 
+                            $tpl = carrega_mensagem_sucesso();
+                            $mensagem_sucesso = "Seus dados acadêmicos foram atualizados em nosso sitemas. Em breve você será redirecionando para a próxima etapa da inscrição.";
+                            $tpl->setVariable('mensagem_sucesso', $mensagem_sucesso);
+                            $tpl_main -> parse('exibe_mensagens');
+                            $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
+                            echo "<meta HTTP-EQUIV='Refresh' CONTENT='5;URL=aluno_escolher_monitoria.php'>";
+                        }
                     }
+                }else{
+                    $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
+                    $tpl = carrega_mensagem_erro();
+                    $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+                    $tpl_main -> parse('exibe_mensagens');
+                    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
                 }
-            }else{
-                $errors[] = "Houve um problema durante a atualização dos seus dados. Tente novamente mais tarde.";
-                $tpl = carrega_mensagem_erro();
-                $tpl->setVariable('mensagem_erros', mostra_erros($errors));
-                $tpl_main -> parse('exibe_mensagens');
-                $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
-            }
+        }
     }
+}else{
+    $errors[] = "Você já finalizou sua inscrição. Não é possível realizar nenhuma mudança.";
+    $tpl = carrega_mensagem_erro();
+    $tpl->setVariable('mensagem_erros', mostra_erros($errors));
+    $tpl_main -> parse('exibe_mensagens');
+    $tpl_main -> setVariable('exibe_mensagens',$tpl->get());
 }
 
 $tpl_main->show();
